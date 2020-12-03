327800e90ebb7f4568ecd8c4024201f44a32ba98
Sascha Koch <sascha.koch@web.de>
2020-12-03 08:36:44 +0100

    day03 refactoring

--- src/aoc_2020/day03.clj
+++ src/aoc_2020/day03.clj
@@ -1,23 +1,14 @@
 (ns aoc-2020.day03
   (:require [clojure.string :as str]))
 
-(defn- element-at [lines x y]
-  (nth (nth lines y) x))
-
 (defn- tree-value [lines [x y]]
-  (let [e (element-at lines x y)]
+  (let [e (nth (nth lines y) x)]
     (cond 
       (= e \#) 1
       :otherwise 0)))
 
-(defn- move-part [row width [x y]]
-  (let [r (mod row 5)]
-    (cond
-      (= r 0) [(mod (inc x) width) (inc y)]
-      (= r 1) [(mod (+ 3 x) width) (inc y)]
-      (= r 2) [(mod (+ 5 x) width) (inc y)]
-      (= r 3) [(mod (+ 7 x) width) (inc y)]
-      (= r 4) [(mod (inc x) width) (+ y 2)])))
+(defn- move [[dx dy] width [x y]]
+    [(mod (+ x dx) width) (+ y dy)])
       
 (defn- count-trees [lines move-pattern]
   (let [width (count (first lines))]
@@ -26,16 +17,16 @@
       (if (>= y (count lines))
         trees
         (recur 
-          (move-part move-pattern width coord)
+          (move move-pattern width coord)
           (+ trees (tree-value lines coord)))))))
 
 (defn part1 [lines] 
-  (count-trees lines 1))
+  (count-trees lines [3 1]))
 
 (defn part2 [lines]
   (apply *
-    (for [r (range 0 5)]
-      (count-trees lines r))))
+    (for [move-pattern [[1 1] [3 1] [5 1] [7 1] [1 2]]]
+      (count-trees lines move-pattern))))
 
 (def lines (str/split-lines (slurp "resources/day03_input.txt")))
 
