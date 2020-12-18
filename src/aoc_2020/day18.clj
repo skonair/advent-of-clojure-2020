edac3d2941aa655081e86004f156b7f716040209
Sascha Koch <sascha.koch@web.de>
2020-12-18 20:22:31 +0100

    day18 part2

--- src/aoc_2020/day18.clj
+++ src/aoc_2020/day18.clj
@@ -1,17 +1,10 @@
 (ns aoc-2020.day18
   (:require [clojure.string :as str]))
 
-; your code here
-
 (defn- prepare-line [line]
   (-> line 
-    (str/replace #" " "")
-    (str/replace #"[(]" "#")
-    (str/replace #"[)]" "(")
-    (str/replace #"[#]" ")")
-    reverse))
+    (str/replace #" " "")))
 
-;
 (defn- get-braced-expression [text]
   (loop [[c & cs] text
          expression [] 
@@ -21,16 +14,19 @@
       (let [newdepth (cond (= c \() (inc depth) (= c \)) (dec depth) :otherwise depth)]
         (recur cs (conj expression c) newdepth)))))
 
-
+(defn- get-number [[c & cs]]
+  (cond 
+    (= c \() (let [[braced rs] (get-braced-expression cs)] [(parse-next braced nil) rs])
+    :otherwise [(- (int c) 48) cs]))
 
 (defn- parse-next [[c & cs :as line] num]
   (if (nil? c)
     num 
     (cond
       (= c \() (let [[braced rs] (get-braced-expression cs)] (parse-next rs (parse-next braced nil)))
-      (= c \)) num
-      (= c \+) (+ num (parse-next cs nil))
-      (= c \*) (* num (parse-next cs nil))
+      (= c \+) (let [[n rs] (get-number cs)] (parse-next rs (+ num n)))
+      (= c \*) (let [[n rs] (get-number cs)] (parse-next rs (* num n))) ; for part1
+   ;   (= c \*) (* num (parse-next cs nil)) ; for part2
       :otherwise (parse-next cs (- (int c) 48)))))
  
 (defn part1 [lines]
@@ -40,15 +36,7 @@
       #(parse-next (prepare-line %) 0)
       lines)))
 
-(defn part2 []
-  (apply 
-    +
-    (map 
-      #(parse-next (prepare-line (str/replace % #"(\d \+ \d)" "($0)")) 0)
-      lines)))
-
-
 (def lines (str/split-lines (slurp "resources/day18_input.txt")))
 
-(println "Day 18 - 1: " (part1 input))
-(println "Day 18 - 2: " (part2 input))
+(println "Day 18 - 1: " (part1 input)) ; 510009915468
+(println "Day 18 - 2: " (part2 input)) ; 321176691637769
