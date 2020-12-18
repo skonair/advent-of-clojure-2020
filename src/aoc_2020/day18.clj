5ec59d6bc1c87d984c17583235b987742385c75c
Sascha Koch <sascha.koch@web.de>
2020-12-18 20:54:53 +0100

    day18 refactoring

--- src/aoc_2020/day18.clj
+++ src/aoc_2020/day18.clj
@@ -14,27 +14,32 @@
       (let [newdepth (cond (= c \() (inc depth) (= c \)) (dec depth) :otherwise depth)]
         (recur cs (conj expression c) newdepth)))))
 
-(defn- get-number [[c & cs]]
+(defn- get-number [[c & cs] part1?]
   (cond 
-    (= c \() (let [[braced rs] (get-braced-expression cs)] [(parse-next braced nil) rs])
+    (= c \() (let [[braced rs] (get-braced-expression cs)] [(parse-next braced nil part1?) rs])
     :otherwise [(- (int c) 48) cs]))
 
-(defn- parse-next [[c & cs :as line] num]
+(defn- parse-next [[c & cs :as line] num part1?]
   (if (nil? c)
     num 
     (cond
-      (= c \() (let [[braced rs] (get-braced-expression cs)] (parse-next rs (parse-next braced nil)))
-      (= c \+) (let [[n rs] (get-number cs)] (parse-next rs (+ num n)))
-      (= c \*) (let [[n rs] (get-number cs)] (parse-next rs (* num n))) ; for part1
-   ;   (= c \*) (* num (parse-next cs nil)) ; for part2
-      :otherwise (parse-next cs (- (int c) 48)))))
- 
-(defn part1 [lines]
+      (= c \+) (let [[n rs] (get-number cs part1?)] (parse-next rs (+ num n) part1?))
+      (and part1? (= c \*)) (let [[n rs] (get-number cs part1?)] (parse-next rs (* num n) part1?)) ; for part1
+      (= c \*) (* num (parse-next cs nil part1?)) ; for part2
+      :otherwise (let [[n rs] (get-number line part1?)] (parse-next rs n part1?)))))
+
+(defn- parse [lines part1?]
   (apply 
     +
     (map 
-      #(parse-next (prepare-line %) 0)
+      #(parse-next (prepare-line %) 0 part1?)
       lines)))
+ 
+(defn part1 [lines]
+  (parse lines true))
+
+(defn part2 [lines]
+  (parse lines false))
 
 (def lines (str/split-lines (slurp "resources/day18_input.txt")))
 
